2cd96a49b747a989890354cae6da0a418c3f16c6
Alex6Crbt <alex6corbillet@gmail.com>
2025-03-31 13:07:44 +0200

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -15,46 +15,28 @@ class Sequence(SequenceWorker):
         self.data_spectro.save_data(self.data_spectro.avg_i, cycle=0, spectype="zero")
         self.data_spectro.zero = self.data_spectro.avg_i
         _ = input("End I_0, place the protein container!!")
-
-        # Main sequence loop
-        for i in range(20):
-            if self.stop_signal:
-                print("Sequence stopped!")
-                break
-
-            print(f"Step {i + 1}/20:")
-            self.reset_servo()
+        print("measuring I_ON-->I_OFF")
+        self.controller.send_command("z")
+        time.sleep(20)
+        self.reset_servo()
+        self.controller.send_command("a")
+        self.controller.send_command("e")
+        # Loop to save I_on data
+        for i in range(100):
             self.data_spectro.lavg.clear()
-            time.sleep(5)
+            time.sleep(0.3)
+            self.data_spectro.save_data(self.data_spectro.avg_i, cycle=0, spectype="on")
+            self.progress_signal.emit(int((i + 1)))
 
-            self.data_spectro.save_data(self.data_spectro.avg_i, cycle=i, spectype="static")
-            self.data_spectro.static = self.data_spectro.avg_i
+        time.sleep(2)
+        self.reset_servo()
+        self.controller.send_command("a")
+        self.controller.send_command("e")
+        self.data_spectro.lavg.clear()
+        time.sleep(10)
 
-            self.controller.send_command("a")
-            self.data_spectro.lavg.clear()
-            time.sleep(5)
-
-            self.controller.send_command("z")
-
-            # Loop to save I_on data
-            for l in range(15):
-                self.data_spectro.lavg.clear()
-                time.sleep(1)
-                self.data_spectro.save_data(self.data_spectro.avg_i, cycle=i, spectype="on")
-
-            self.controller.send_command("Z")
-            time.sleep(5)
-            self.controller.send_command("e")
-
-            # Loop to save I_off data
-            for l in range(45):
-                self.data_spectro.lavg.clear()
-                time.sleep(1)
-                self.data_spectro.save_data(self.data_spectro.avg_i, cycle=i, spectype="off")
-            time.sleep(2)
-
-            # Update progress
-            self.progress_signal.emit(int((i + 1) * 5))
+        self.data_spectro.save_data(self.data_spectro.avg_i, cycle=0, spectype="static")
+        # Update progress
 
         print("Sequence finished")
         self.finished_signal.emit()  # Emit finished signal

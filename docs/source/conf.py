8701709d4d7380e8690f639286f8e8cc48cbaec3
Alex6Crbt <alex6corbillet@gmail.com>
2025-04-02 00:30:38 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,10 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
+try:
+    import XChroma.app
+except ImportError as e:
+    raise ImportError("Failed to import XChroma.app: " + str(e))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
